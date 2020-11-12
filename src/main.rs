0f8e891e45cd3e1d5c8a0f25fb427ea5299d2b6b
Ashley <admin@trapacid.icu>
2020-11-12 08:26:44 -0500

    CLI args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::io;
+use std::env;
+
 fn celsius(f: f64) -> f64 { // f being Fahrenheit
     let y = (f - 32.0) * 5.0/9.0;
     let formatted = (y * 100.0).round() / 100.0;
@@ -17,15 +20,26 @@ fn kelvin(c: f64) -> f64 { // c being Celsius
 }
 
 fn main() {
-    use std::io;
+    let args: Vec<String> = env::args().collect();
+    let args_len = args.len();
 
-    let mut temperature = String::new();
-    println!("Enter a number:\n");
-    io::stdin().read_line(&mut temperature).expect("Failed to read input.");
-
-    let temperature: f64 = temperature.trim().parse().expect("Please enter a number.");
-    
-    println!("Celsius: {}°", celsius(temperature));
-    println!("Fahrenheit: {}°", fahrenheit(temperature));
-    println!("Kelvin: {}°", kelvin(temperature));
+    if args_len >= 2 {
+        let temperature = String::from(&args[1]);
+        let temperature: f64 = temperature.trim().parse().expect("you shouldn't see this");
+        println!("Celsius: {}°", celsius(temperature));
+        println!("Fahrenheit: {}°", fahrenheit(temperature));
+        println!("Kelvin: {}°", kelvin(temperature));
+    }
+    else {
+        let mut temperature = String::new();
+        println!("{:?}", args);
+        println!("Enter a number:\n");
+        io::stdin().read_line(&mut temperature).expect("Failed to read input.");
+        
+        let temperature: f64 = temperature.trim().parse().expect("Please enter a number.");
+        
+        println!("Celsius: {}°", celsius(temperature));
+        println!("Fahrenheit: {}°", fahrenheit(temperature));
+        println!("Kelvin: {}°", kelvin(temperature));
+    }
 }
